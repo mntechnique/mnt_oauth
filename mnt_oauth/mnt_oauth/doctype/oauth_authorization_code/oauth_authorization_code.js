876af013ae0910a7ea51669d7e67da2d09c1ddd6
Gaurav Naik <gvrvnk@gmail.com>
2016-11-05 14:45:46 +0530

    OAuth doctypes removed. Now in core Frappe.

--- mnt_oauth/mnt_oauth/doctype/oauth_authorization_code/oauth_authorization_code.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2016, MN Technique and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('OAuth Authorization Code', {
-	refresh: function(frm) {
-
-	}
-});
